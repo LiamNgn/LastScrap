37074d02ef338107d97509dd188f22697c2495ba
Tung Lam Nguyen Le <dragontunglam@gmail.com>
2021-06-21 17:59:26 +0700

    Feat: update spider to scrap information about inmate

--- LIS/LIS/spiders/spider.py
+++ LIS/LIS/spiders/spider.py
@@ -8,6 +8,16 @@ class StackSpider(Spider):
 	]
 
 	def parse(self, response):
+		for info in response.xpath("//tr[td[a[contains(text(),'Last Statement')]]]"):
+			yield{
+			"Last Name": info.xpath('td[4]/text()').get(),
+			"First Name": info.xpath('td[5]/text()').get(),
+			"TDCJ number": info.xpath('td[6]/text()').get(),
+			"Age": info.xpath('td[7]/text()').get(),
+			"Date of Execution": info.xpath('td[8]/text()').get(),
+			"Race": info.xpath('td[9]/text()').get(),
+			"County": info.xpath('td[10]/text()').get(),
+			}
 		for quotes in response.xpath('//td//a[contains(@href,"last.html")]/@href'):
 			url = response.urljoin(quotes.extract())
 			yield scrapy.Request(url, callback = self.parse_dir_content)
@@ -19,3 +29,4 @@ class StackSpider(Spider):
 				'Inmate:': quotes.xpath('//p[4]').get(),
 				'Last Statement:': quotes.xpath('//p[6]').get()
 				}
+

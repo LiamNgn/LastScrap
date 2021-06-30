d0eff74855820531faa73298f91847383370e0a8
Tung Lam Nguyen Le <dragontunglam@gmail.com>
2021-06-30 14:08:09 +0700

    Update spider to include ID info in last statement crawl to link with inmate infomation using ID by meta in scrapy.requests

--- LIS/LIS/spiders/spider.py
+++ LIS/LIS/spiders/spider.py
@@ -10,6 +10,7 @@ class StackSpider(Spider):
 	def parse(self, response):
 		for info in response.xpath("//tr[td[a[contains(text(),'Last Statement')]]]"):
 			yield{
+			"ID:": info.xpath('td[1]/text()').get(),
 			"Last Name": info.xpath('td[4]/text()').get(),
 			"First Name": info.xpath('td[5]/text()').get(),
 			"TDCJ number": info.xpath('td[6]/text()').get(),
@@ -18,15 +19,20 @@ class StackSpider(Spider):
 			"Race": info.xpath('td[9]/text()').get(),
 			"County": info.xpath('td[10]/text()').get(),
 			}
-		for quotes in response.xpath('//td//a[contains(@href,"last.html")]/@href'):
-			url = response.urljoin(quotes.extract())
-			yield scrapy.Request(url, callback = self.parse_dir_content)
+		for quotes in response.xpath("//tr[td[a[contains(text(),'Last Statement')]]]"):
+			url = response.urljoin(quotes.xpath('td[3]/a/@href').extract()[0])
+			yield scrapy.Request(url, callback = self.parse_dir_content, meta = {"ID": quotes.xpath('td[1]/text()').get()})
+
+
+
 
 	def parse_dir_content(self,response):
-		for quotes in response.xpath('//div[@id="content_right"]'):
+		#for quotes in response.xpath('//div[@id="content_right"]'):
+			quotes = response.xpath('//div[@id="content_right"]')
 			yield{
-				'Date of Execution:': quotes.xpath('//p[2]').get(),
-				'Inmate:': quotes.xpath('//p[4]').get(),
-				'Last Statement:': quotes.xpath('//p[6]').get()
+				'ID':response.meta["ID"],
+				'Date of Execution:': quotes.xpath('//p[2]/text()').get(),
+				'Inmate:': quotes.xpath('//p[4]/text()').get(),
+				'Last Statement:': quotes.xpath('//p[6]/text()').get()
 				}
 
